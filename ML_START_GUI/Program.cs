c3e88ab2574919d2d10c1331299e9b960506c131
Andrew_Ln <linnandrew663@gmail.com>
2024-02-29 10:39:28 +0300

    v2.3a config transfered to the Server project

--- ML_START_GUI/Program.cs
+++ ML_START_GUI/Program.cs
@@ -11,60 +11,24 @@ using LoggingLibrary;
 using static Serilog.Events.LogEventLevel;
 
 
-namespace MLSTART_GUI
+namespace MLSTART_GUI;
+
+internal class Program
 {
-    internal record Configuration(int NameLength, int LastNameLength, int ActionDelay);
-
-    internal class Program
+    [STAThread]
+    public static void Main(string[] args)
     {
-        internal static Configuration Config { get; private set; } = null!;
+        LoggingTool.WriteToFile(Debug, Information, Warning, Error);
 
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            LoggingTool.WriteToFile(Debug, Information, Warning, Error);
-
-            #region creating or reading config file
-            string configFile = "config.json";
-            if (!File.Exists(configFile))
-            {
-                var config = new Dictionary<string, object>()
-                {
-                    ["NameLength"] = 6,
-                    ["LastNameLength"] = 6,
-                    ["ActionDelay"] = 500
-                };
-                string configText = JsonSerializer.Serialize(config);
-
-                File.WriteAllText(configFile, configText);
-            }
-            string configFileContent = File.ReadAllText(configFile);
-
-            try
-            {
-                Config = JsonSerializer
-                    .Deserialize<Configuration>(configFileContent)!;
-            }
-            catch (JsonException ex)
-            {
-                LoggingTool.LogByTemplate(Error, ex, $"Чтение данных из файла {configFile} вызвало ошибку");
-            }
-            catch (NullReferenceException ex)
-            {
-                LoggingTool.LogByTemplate(Error, ex, $"Чтение данных из файла {configFile} вызвало ошибку");
-            }
-            #endregion
-
-            BuildAvaloniaApp()
-                .StartWithClassicDesktopLifetime(args);
-        }
-
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace()
-                .UseReactiveUI();
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
     }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
 
 using Avalonia;
 using Avalonia.ReactiveUI;
@@ -6,27 +9,63 @@ using Avalonia.ReactiveUI;
 using LoggingLibrary;
 using static Serilog.Events.LogEventLevel;
 
-namespace Server
-{
-    internal class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            LoggingTool.WriteToFile(Debug, Information, Warning, Error);
-            BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-        }
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace()
-                .UseReactiveUI();
+namespace Server;
+internal record Configuration(int NameLength, int LastNameLength, int ActionDelay, string ConnectionString);
+internal class Program
+{
+    internal static Configuration Config { get; private set; } = null!;
+
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        LoggingTool.WriteToFile(Debug, Information, Warning, Error);
+
+        #region creating or reading config file
+        string configFile = "config.json";
+        if (!File.Exists(configFile))
+        {
+            var config = new Dictionary<string, object>()
+            {
+                ["NameLength"] = 6,
+                ["LastNameLength"] = 6,
+                ["ActionDelay"] = 500,
+                ["ConnectionString"] = "Server=(localdb)\\MSSQLLocalDB;Database=Test;Trusted_Connection=True;TrustServerCertificate=True"
+            };
+            string configText = JsonSerializer.Serialize(config);
+
+            File.WriteAllText(configFile, configText);
+        }
+        string configFileContent = File.ReadAllText(configFile);
+
+        try
+        {
+            Config = JsonSerializer
+                .Deserialize<Configuration>(configFileContent)!;
+        }
+        catch (JsonException ex)
+        {
+            LoggingTool.LogByTemplate(Error, ex, $"Чтение данных из файла {configFile} вызвало ошибку");
+        }
+        catch (NullReferenceException ex)
+        {
+            LoggingTool.LogByTemplate(Error, ex, $"Чтение данных из файла {configFile} вызвало ошибку");
+        }
+        catch (Exception ex)
+        {
+            LoggingTool.LogByTemplate(Error, ex, $"Чтение данных из файла {configFile} вызвало непредвиденную ошибку");
+        }
+        #endregion
+
+        BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
     }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
 }
