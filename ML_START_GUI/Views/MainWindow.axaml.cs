6bf8b34306460cbcc21f3eb9ed4eef696116dcbf
Andrew_Ln <linnandrew663@gmail.com>
2024-03-06 15:51:23 +0300

    v2.9a MainWindow navigation enhanced (navigation on arrows and click event on enter)

--- ML_START_GUI/Views/MainWindow.axaml.cs
+++ ML_START_GUI/Views/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Input;
+using Avalonia.Media;
 
 namespace MLSTART_GUI.Views;
 
@@ -7,44 +12,133 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        authentificationWindow.Loaded += AuthentificationWindow_Loaded;
+        Initialized += MainWindow_Initialized;
 
-        // TODO: мб сделать перемещение менее костыльным
+        SignInButton.KeyDown += MenuButton_Click;
+        SignUpButton.KeyDown += MenuButton_Click;
+        ResetButton.KeyDown += MenuButton_Click;
+
+        #region Button Focus events
+        SignInButton.GotFocus += MenuButton_GotFocus;
+        SignUpButton.GotFocus += MenuButton_GotFocus;
+        ResetButton.GotFocus += MenuButton_GotFocus;
+
+        SignInButton.LostFocus += MenuButton_LostFocus; 
+        SignUpButton.LostFocus += MenuButton_LostFocus;
+        ResetButton.LostFocus += MenuButton_LostFocus;
+        #endregion
+
+        #region Control navigation
         LoginField.KeyDown += LoginField_KeyDown;
         PasswordField.KeyDown += PasswordField_KeyDown;
         RepeatPasswordField.KeyDown += RepeatPasswordField_KeyDown;
+
+        SignInButton.KeyDown += SignInButton_KeyDown;
+        SignUpButton.KeyDown += SignUpButton_KeyDown;
+        ResetButton.KeyDown += ResetButton_KeyDown;
+        #endregion
     }
 
-    private void AuthentificationWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void MainWindow_Initialized(object? sender, EventArgs e)
     {
         LoginField.Focus();
     }
 
-    private void RepeatPasswordField_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    private void MenuButton_Click(object? sender, KeyEventArgs e)
     {
-        if (e.Key == Avalonia.Input.Key.Up)
+        if (e.Key == Key.Enter && sender is Button button)
+        {
+            var clickEvent = new RoutedEventArgs(Button.ClickEvent);
+            button.RaiseEvent(clickEvent);
+        }
+    }
+
+    private void MenuButton_LostFocus(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button)
+        {
+            button.BorderThickness = Thickness.Parse("0");
+        }
+    }
+
+    private void MenuButton_GotFocus(object? sender, GotFocusEventArgs e)
+    {
+        if (sender is Button button)
+        {
+            button.BorderThickness = Thickness.Parse("2");
+            button.BorderBrush = Brushes.Black;
+        }
+    }
+
+    #region KeyDown event handlers
+    // TextBoxes
+    private void LoginField_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Down)
         {
             PasswordField.Focus();
         }
     }
 
-    private void PasswordField_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    private void PasswordField_KeyDown(object? sender, KeyEventArgs e)  
     {
-        if (e.Key == Avalonia.Input.Key.Down)
+        if (e.Key == Key.Down)
         {
             RepeatPasswordField.Focus();
         }
-        else if (e.Key == Avalonia.Input.Key.Up)
+        else if (e.Key == Key.Up)
         {
             LoginField.Focus();
         }
     }
 
-    private void LoginField_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    private void RepeatPasswordField_KeyDown(object? sender, KeyEventArgs e)
     {
-        if (e.Key == Avalonia.Input.Key.Down)
+        if (e.Key == Key.Up)
         {
             PasswordField.Focus();
         }
+        else if (e.Key == Key.Down)
+        {
+            SignInButton.Focus();
+        }
     }
+
+    // Buttons
+    private void SignInButton_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Up)
+        {
+            RepeatPasswordField.Focus();
+        }
+        else if (e.Key == Key.Down)
+        {
+            SignUpButton.Focus();
+        }
+    }
+
+    private void SignUpButton_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Up)
+        {
+            SignInButton.Focus();
+        }
+        else if (e.Key == Key.Down)
+        {
+            ResetButton.Focus();
+        }
+    }
+
+    private void ResetButton_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Up)
+        {
+            SignUpButton.Focus();
+        }
+        else if (e.Key == Key.Down)
+        {
+            LoginField.Focus();
+        }
+    }
+    #endregion
 }
